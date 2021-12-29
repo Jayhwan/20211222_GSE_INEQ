8e40778bcce36a466948d96ac49fccbbe177fac2
Jayhwan <jihwan14@kaist.ac.kr>
2021-12-29 17:15:30 +0900

    Completed code for EV problems and test data

--- Test.py
+++ Test.py
@@ -1,15 +1,31 @@
-import numpy as np
+from Prob_Multi_Hour_EV import *
+import matplotlib.pyplot as plt
 
-x = np.random.random((3,5,4))
-y = np.random.random((1,5,4))
-z = np.random.random((1,5,4))
-l = [x, y, z]
+with open('grad.pkl', 'rb') as f:
+    prob1 = pickle.load(f)
 
-m = x
-print(m.shape)
-for i in range(1, len(l)):
-    m = np.vstack((m, l[i]))
-    print(m.shape)
+with open('prox.pkl', 'rb') as f:
+    prob2 = pickle.load(f)
 
 
-print("MODIFY")
+print("Gradient Utility Changes\n", prob1.leader_utility_history)
+print("Proximal Utility Changes\n", prob2.leader_utility_history)
+
+u1 = prob1.leader_utility_history
+u2 = prob2.leader_utility_history
+print(u1)
+if len(u1) < len(u2):
+    while len(u1) != len(u2):
+        u1 += [u1[-1]]
+else:
+    while len(u1) != len(u2):
+        u2 += [u2[-1]]
+
+plt.figure()
+plt.plot(u1[:40], color='r', label='GRAD')
+plt.plot(u2[:40], color='b', label='FROX')
+plt.xlabel("Iteration")
+plt.ylabel("Leader Utility")
+plt.legend()
+plt.show()
+
